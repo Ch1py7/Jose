88e99d369e938bd1a5c7b12e1bea07509145721b
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-06-17 01:49:10 -0600

    feat: add nickname setting for 'GAY OF THE DAY' role assignment and removal

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const client = new Client({
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.GuildVoiceStates,
 		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildModeration,
 	],
 })
 
